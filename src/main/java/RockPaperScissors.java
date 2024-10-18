eafc65c0034f0644f32fb2af9ec9ddfdbd58a24d
Anish Nag <anishnag01@gmail.com>
2024-10-18 12:52:57 -0500

    test commit

---
+++ src/main/java/RockPaperScissors.java
@@ -0,0 +1,10 @@
+// This is a game for rock paper and scissors
+
+
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+        String[] rps = {"rock", "paper", "scissors"};
+        
+    }
+}
