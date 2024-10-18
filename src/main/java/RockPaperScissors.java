499be6d05c1fb58d78d0b5e1556febc99da640d6
Anish Nag <anishnag01@gmail.com>
2024-10-18 13:54:35 -0500

    fixed code

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -2,58 +2,61 @@
 import java.util.Scanner;
 import java.util.Random;
 
-
-
-
 public class RockPaperScissors {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
         while (true) {
-            String[] rps = {"rock", "paper", "scissors" };
-            String computerMove = rps[new.Random().nextInt(rps.length)];
-
-
+            String[] rps = {"rock", "paper", "scissors"};
+            String computerMove = rps[random.nextInt(rps.length)];
             String playerMove;
+
             while (true) {
-                System.out.println("Please enter your move (rock, paper, or scissors)");
-                playerMove = scanner.nextLine();
+                System.out.println("Please enter your move (rock, paper, or scissors):");
+                playerMove = scanner.nextLine().toLowerCase();
                 if (playerMove.equals("rock") || playerMove.equals("paper") || playerMove.equals("scissors")) {
                     break;
                 }
-                System.out.println(playerMove + " is not a valid move.");
+                System.out.println("Invalid move. Please try again.");
             }
+
             System.out.println("Computer played: " + computerMove);
+
             if (playerMove.equals(computerMove)) {
-                System.out.println("This is a tie");
-            } else if (playerMove.equals("rock")) {
-                if (computerMove.equals("paper")) {
-                    System.out.println("You lose!");
-                } else if (computerMove.equals("scissors")) {
-                    System.out.println("You win!");
-                }
-
-            } else if (playerMove.equals("paper")) {
-                if (computerMove.equals("rock")) {
-                    System.out.println("You win!");
-                } else if (computerMove.equals("scissors")) {
-                    System.out.println("You lose!");
-                }
-
-            } else if (playerMove.equals("scissors")) {
-                if (computerMove.equals("paper")) {
-                    System.out.println("You win!");
-                } else if (computerMove.equals("rock")) {
-                    System.out.println("You lose!");
-                }
-
+                System.out.println("The game was a tie!");
             }
-            System.out.println("Play again?(Y/N)");
-            String playAgain = scanner.nextLine();
+            else if (playerMove.equals("rock")) {
+                if (computerMove.equals("paper")) {
+                    System.out.println("You lose!");
+                } else if (computerMove.equals("scissors")) {
+                    System.out.println("You win!");
+                }
+            }
+            else if (playerMove.equals("paper")) {
+                if (computerMove.equals("scissors")) {
+                    System.out.println("You lose!");
+                } else if (computerMove.equals("rock")) {
+                    System.out.println("You win!");
+                }
+            }
+            else if (playerMove.equals("scissors")) {
+                if (computerMove.equals("rock")) {
+                    System.out.println("You lose!");
+                } else if (computerMove.equals("paper")) {
+                    System.out.println("You win!");
+                }
+            }
 
-            if(!playAgain.equals("Y")){
+            System.out.println("Play again? (yes/no)");
+            String playAgain = scanner.nextLine().toLowerCase();
+            if (!playAgain.equals("yes")) {
                 break;
             }
         }
+
+        System.out.println("Thanks for playing!");
+        scanner.close();
     }
 }
